7b345fb2865717f0bc8987b0e21007faa5e0aed1
SAKLAYEN TANIM <astanim.3221@gmail.com>
2020-09-19 18:46:20 +0600

    Upload on 19-09-2020
    This is integrating process of bKash payment gateway in your application according to bKash official developer website.

---
+++ head.php
@@ -0,0 +1,16 @@
+   <head>
+      <meta name="viewport" content="width=device-width" , initial-scale=1.0/>
+      <script src="https://merchantdemo.sandbox.bka.sh/frontend/resource/js/prism.js"></script>
+      <script src="https://merchantdemo.sandbox.bka.sh/frontend/resource/js/jquery.js"></script>
+      <!-- JavaScript -->
+      <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.11.2/build/alertify.min.js"></script>
+      <script src="https://merchantdemo.sandbox.bka.sh/frontend/resource/js/site.common.js"></script>
+      <link rel="stylesheet" href="https://merchantdemo.sandbox.bka.sh/frontend/resource/css/prism.css">
+      <link href="https://merchantdemo.sandbox.bka.sh/frontend/resource/css/flatly.bootstrap.4.css" rel="stylesheet"/>
+      <link href="https://merchantdemo.sandbox.bka.sh/frontend/resource/css/flatly.custom.css" rel="stylesheet"/>
+      <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.2/build/css/alertify.min.css"/>
+      <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.2/build/css/themes/bootstrap.min.css"/>
+      <link href="https://merchantdemo.sandbox.bka.sh/frontend/resource/css/checkout.css" rel="stylesheet"/>
+	  
+	  <script src="https://scripts.sandbox.bka.sh/versions/1.1.0-beta/checkout/bKash-checkout-sandbox.js"></script>
+   </head>
